7e34c4bf53db28eaf3710d4258d56c82a7d42cc8
Michiel Borkent <michielborkent@gmail.com>
2023-02-10 11:37:23 +0100

    Pick right basis file

--- src/eval/deps_try.clj
+++ src/eval/deps_try.clj
@@ -1,11 +1,12 @@
 (ns eval.deps-try
-  (:require [babashka.classpath :refer [get-classpath]]
-            [babashka.deps :as deps]
-            [babashka.process :as p :refer [sh]]
-            [clojure.string :as str]))
+  {:clj-kondo/config '{:lint-as {babashka.fs/with-temp-dir clojure.core/let}}}
+  (:require
+   [babashka.classpath :as cp :refer [get-classpath]]
+   [babashka.deps :as deps]
+   [babashka.process :as p]
+   [clojure.string :as str]))
 
 (def init-cp (get-classpath))
-
 
 (deps/add-deps '{:deps {org.clojure/tools.gitlibs {:mvn/version "2.4.181"}}})
 
@@ -13,15 +14,22 @@
          '[babashka.fs :as fs] :reload
          '[babashka.http-client] :reload) ;; reload so we use the dep, not the built-in
 
+(defn parse-cp-file [s]
+  (some #(when (str/includes? % "cp_file")
+           (str/trim (second (str/split % #"=")))) (str/split-lines s)))
+
+(defn deps->cp [tmp deps]
+  (str/trim (with-out-str (deps/clojure {:dir (str tmp)} "-Spath" "-Sdeps" (str {:deps deps})))))
 
 (defn -main [& args]
-  (let [cache-dir      (fs/with-temp-dir [tmp {}]
-                         (:cache-dir (read-string (:out (sh ["clojure" "-Sdescribe"] {:dir (str tmp)})))))
-        requested-deps (try-deps/parse-dep-args args)
-        deps->cp       #(fs/with-temp-dir [tmp {}]
-                          (str/trim (:out (sh ["clojure" "-Spath" "-Sdeps" (str {:deps %})] {:dir (str tmp)}))))
-        default-cp     (deps->cp '{org.clojure/clojure {:mvn/version "RELEASE"}})
-        requested-cp   (deps->cp requested-deps)]
-    (p/exec ["java" "-classpath" (str default-cp fs/path-separator init-cp fs/path-separator requested-cp)
-             (str "-Dclojure.basis=" (first (fs/glob cache-dir "*.basis")))
-             "clojure.main" "-m" "eval.deps-try.try"])))
+  (fs/with-temp-dir [tmp {}]
+    (let [verbose-output (with-out-str (deps/clojure {:dir (str tmp)} "-Sverbose" "-Spath"))
+          cp-file (parse-cp-file verbose-output)
+          basis-file (str/replace cp-file #".cp$" ".basis")
+          requested-deps (try-deps/parse-dep-args args)
+          default-cp (deps->cp tmp '{org.clojure/clojure {:mvn/version "RELEASE"}})
+          requested-cp (deps->cp tmp requested-deps)
+          classpath (str default-cp fs/path-separator init-cp fs/path-separator requested-cp)]
+      (p/exec "java" "-classpath" classpath
+              (str "-Dclojure.basis=" basis-file)
+              "clojure.main" "-m" "eval.deps-try.try"))))
