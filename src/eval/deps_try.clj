6110a59a5b8c2bd5c4bab7ff4694557d19df31e6
Gert Goet <gert@thinkcreate.dk>
2023-02-15 15:12:56 +0100

    -h/--help/help
    and show error when first arg doesn't look like a dep

--- src/eval/deps_try.clj
+++ src/eval/deps_try.clj
@@ -21,15 +21,56 @@
 (defn deps->cp [tmp deps]
   (str/trim (with-out-str (deps/clojure {:dir (str tmp)} "-Spath" "-Sdeps" (str {:deps deps})))))
 
+(defn print-usage []
+  (println "Usage:
+  deps-try [dep-name [dep-version] [dep2-name ...] ...]
+
+Example:
+# A REPL using the latest Clojure version
+$ deps-try
+
+# A REPL with specific dependencies (latest version implied)
+$ deps-try metosin/malli criterium/criterium
+
+# ...specific version
+$ deps-try metosin/malli 0.9.2
+
+# Dependency from GitHub/GitLab/SourceHut (gets you the latest SHA from the default branch)
+$ deps-try https://github.com/metosin/malli
+
+# ...a specific branch/SHA
+$ deps-try https://github.com/metosin/malli some-branch-sha-or-tag
+
+# ...using the 'infer' notation, e.g.
+# com.github.<user>/<project>, com.gitlab.<user>/<project>, ht.sr.<user>/<project>
+$ deps-try com.github.metosin/malli
+
+During a REPL-session:
+# see help for all options
+user=> :repl/help
+
+# add dependencies
+user=> :deps/try dev.weavejester/medley
+"))
+
+(defn- print-usage? [args]
+  (contains? #{"-h" "--help" "help"} (first args)))
+
+(defn- invalid-args? [args]
+  (when-let [[some-dep] (not-empty args)]
+    (not (re-seq #"/" some-dep))))
+
 (defn -main [& args]
-  (fs/with-temp-dir [tmp {}]
-    (let [verbose-output (with-out-str (deps/clojure {:dir (str tmp)} "-Sverbose" "-Spath"))
-          cp-file (parse-cp-file verbose-output)
-          basis-file (str/replace cp-file #".cp$" ".basis")
-          requested-deps (try-deps/parse-dep-args args)
-          default-cp (deps->cp tmp '{org.clojure/clojure {:mvn/version "RELEASE"}})
-          requested-cp (deps->cp tmp requested-deps)
-          classpath (str default-cp fs/path-separator init-cp fs/path-separator requested-cp)]
-      (p/exec "java" "-classpath" classpath
-              (str "-Dclojure.basis=" basis-file)
-              "clojure.main" "-m" "eval.deps-try.try"))))
+  (if (or (print-usage? args) (invalid-args? args))
+    (print-usage)
+    (fs/with-temp-dir [tmp {}]
+      (let [verbose-output (with-out-str (deps/clojure {:dir (str tmp)} "-Sverbose" "-Spath"))
+            cp-file (parse-cp-file verbose-output)
+            basis-file (str/replace cp-file #".cp$" ".basis")
+            requested-deps (try-deps/parse-dep-args args)
+            default-cp (deps->cp tmp '{org.clojure/clojure {:mvn/version "RELEASE"}})
+            requested-cp (deps->cp tmp requested-deps)
+            classpath (str default-cp fs/path-separator init-cp fs/path-separator requested-cp)]
+        (p/exec "java" "-classpath" classpath
+                (str "-Dclojure.basis=" basis-file)
+                "clojure.main" "-m" "eval.deps-try.try")))))
