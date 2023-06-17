599db48734a34813b24f9f7ab080cc9c05d79af6
Gert Goet <gert@thinkcreate.dk>
2023-06-17 22:37:11 +0200

    Allow to override default deps
    e.g. change clojure version

--- src/eval/deps_try.clj
+++ src/eval/deps_try.clj
@@ -136,8 +136,9 @@ user=> :repl/help
             basis-file                   (str/replace cp-file #".cp$" ".basis")
             requested-cp                 (deps->cp tmp requested-deps)
             classpath                    (str (fs/cwd) fs/path-separator
+                                              requested-cp fs/path-separator
                                               default-cp fs/path-separator
-                                              init-cp fs/path-separator requested-cp)]
+                                              init-cp)]
         (warn-unless-minimum-clojure-cli-version "1.11.1.1273" tdeps-version)
         (p/exec "java" "-classpath" classpath
                 (str "-Dclojure.basis=" basis-file)
