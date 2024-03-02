fab2e43bf5edef1e87753d82dba4e185708061bd
Gert Goet <gert@thinkcreate.dk>
2024-03-02 11:12:07 +0100

    Wording

--- src/eval/deps_try.clj
+++ src/eval/deps_try.clj
@@ -67,7 +67,7 @@
                     "  ;; The latest version of malli from maven, and git-tag v1.3.894 of the next-jdbc repository" \newline
                     "  $ deps-try metosin/malli io.github.seancorfield/next-jdbc v1.3.894")
                (str ansi/bold "COMMANDS" ansi/reset \newline
-                    "  recipes    show list of recipes (--refresh to update manifest)")
+                    "  recipes    list built-in recipes (--refresh to update)")
                nil]]
     (print (string/join \newline (interpose nil usage)))))
 
